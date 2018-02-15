8ccda8f4516d192cfe69665e794f4772fceeafaa
yoda <yoda@yoda.local>
2018-02-15 13:54:39 +0900

    refactor

--- src/application/score.rb
+++ src/application/score.rb
@@ -14,9 +14,7 @@ class Score
 
   def save
     file_exist
-    File.open($path) do |i|
-      @results = JSON.load(i)
-    end
+    @results = Score.all
     @results.store("#{person}", point)
     File.open($path, 'w') do |j|
       JSON.dump(@results, j)
@@ -25,9 +23,7 @@ class Score
 
   def self.delete(line_num)
     @results = Score.all
-    @results.delete_if.with_index{|(k, v), i|
-      i == line_num - 1
-    }
+    @results.delete_if.with_index{|(k, v), i| i == line_num - 1}
     File.open($path, 'w') do |i|
       JSON.dump(@results, i)
     end
